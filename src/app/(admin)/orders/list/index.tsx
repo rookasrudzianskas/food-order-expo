17338e561c7ad14ace6750afe648e6002ca70cad
rokas <rokas@rokas.com>
2024-02-08 17:38:05 +0200

    Add real-time update functionality for orders

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -2,14 +2,35 @@ import { FlatList } from 'react-native-gesture-handler';
 import { Stack } from 'expo-router';
 import OrderListItem from "@/src/components/order-list-item";
 import {useAdminOrderList} from "@/src/api/orders";
-import React from "react";
+import React, {useEffect} from "react";
 import IsLoading from "@/src/components/ui/is-loading";
 import ErrorAPI from "@/src/components/ui/error-api";
+import {supabase} from "@/src/app/lib/supabase";
+import {useQueryClient} from "@tanstack/react-query";
 
 export default function OrdersScreen() {
   const { data: orders, isLoading, error } = useAdminOrderList({ archived: false });
   if(isLoading) return <IsLoading />
   if(error) return <ErrorAPI error={error} />
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    const orders = supabase
+      .channel('custom-insert-channel')
+      .on(
+        'postgres_changes',
+        { event: 'INSERT', schema: 'public', table: 'orders' },
+        (payload) => {
+          console.log('Change received!', payload);
+          // @ts-ignore
+          queryClient.invalidateQueries(['orders']);
+        }
+      )
+      .subscribe();
+    return () => {
+      orders.unsubscribe();
+    };
+  }, [])
 
   return (
     <>
