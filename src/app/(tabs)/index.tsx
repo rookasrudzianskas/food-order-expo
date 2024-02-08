58b2d7cb9d59ad02fc24ca53aeb1751ffefc0e1a
rokas <rokas@rokas.com>
2024-02-08 09:27:48 +0200

    Update Nativewind v4 starter to Nativewind v4

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,7 +2,6 @@
 import { vars, useColorScheme } from "nativewind";
 import { Pressable, Text, View, PressableProps } from "react-native";
 import { cva, type VariantProps } from "class-variance-authority";
-import { cn } from "@/lib/utils";
 import React from "react";
 
 const TabOneScreen = () => {
@@ -10,7 +9,7 @@ const TabOneScreen = () => {
     <View className="bg-gray-100 flex-1 p-3">
       <View className="flex-1 bg-white rounded-xl pt-6 items-center justify-center">
         <Text className="text-2xl uppercase font-bold text-gray-700 text-center">
-          Nativewind v4 starter
+          Nativewind v4
         </Text>
       </View>
     </View>
