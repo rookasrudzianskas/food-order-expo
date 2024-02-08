18c518c98f1711f9de17d6c247a50df725942bf6
rokas <rokas@rokas.com>
2024-02-08 14:29:04 +0200

    Add Supabase library and dependencies

---
+++ src/app/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://yiztfonkkmubplgvrhga.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpenRmb25ra211YnBsZ3ZyaGdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczOTUyNDAsImV4cCI6MjAyMjk3MTI0MH0.c7fkAFH0dPFtkRnmcAWJmwmBC1LWHAOYlkXhl-lhlZI';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
