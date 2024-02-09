6a1f26216403360c880c55356c0480a4bcf05068
rokas <rokas@rokas.com>
2024-02-09 08:24:20 +0200

    Add supabase util to create or retrieve profile

---
+++ supabase/functions/payment-sheet/_utils/supabase.ts
@@ -0,0 +1,21 @@
+// @ts-nocheck
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+export const createOrRetrieveProfile = async (req: Request) => {
+  const supabaseClient = createClient(
+    Deno.env.get('SUPABASE_URL') ?? '',
+    Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+    {
+      global: {
+        headers: { Authorization: req.headers.get('Authorization')! },
+      },
+    }
+  );
+  // Now we can get the session or user object
+  const {
+    data: { user },
+  } = await supabaseClient.auth.getUser();
+
+  console.log(user);
+  if (!user) throw new Error('No user found');
+};
