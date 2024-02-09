0b79e791dabdf520502246025944e398d129b047
rokas <rokas@rokas.com>
2024-02-09 08:05:41 +0200

    Update paymentIntent amount to use the variable 'amount'

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -10,7 +10,7 @@ serve(async (req) => {
 
     // Create a PaymentIntent so that the SDK can charge the logged in customer.
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: 1099,
+      amount: amount,
       currency: 'usd',
       // customer: customer,
     });
