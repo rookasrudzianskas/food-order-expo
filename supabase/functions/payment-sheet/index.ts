bcc4554a1f385a481b43daf4bbb42ed6c5f64d34
rokas <rokas@rokas.com>
2024-02-09 08:30:18 +0200

    Create ephemeral key for customer's payment methods

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -1,30 +1,40 @@
 // @ts-nocheck
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
+import {createOrRetrieveProfile} from "@/supabase/functions/payment-sheet/_utils/supabase";
 import {stripe} from "@/supabase/functions/payment-sheet/_utils/stripe";
 
 console.log('Hello from Functions!');
 
-serve(async (req) => {
+serve(async (req: Request) => {
   try {
     const { amount } = await req.json();
 
-    // Create a PaymentIntent so that the SDK can charge the logged in customer.
+    const customer = await createOrRetrieveProfile(req);
+
+    // Create an ephermeralKey so that the Stripe SDK can fetch the customer's stored payment methods.
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer },
+      { apiVersion: '2020-08-27' }
+    );
+
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: 'usd',
-      // customer: customer,
+      customer: customer,
     });
+
     const res = {
-      publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
       paymentIntent: paymentIntent.client_secret,
-      // ephemeralKey: ephemeralKey.secret,
-      // customer: customer,
+      publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
+      customer: customer,
+      ephemeralKey: ephemeralKey.secret,
     };
+
     return new Response(JSON.stringify(res), {
       headers: { 'Content-Type': 'application/json' },
-      status: 200,
     });
   } catch (error) {
+    console.log(error);
     return new Response(JSON.stringify(error), {
       headers: { 'Content-Type': 'application/json' },
       status: 400,
@@ -32,14 +42,8 @@ serve(async (req) => {
   }
 });
 
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Rokas"}'
-
-*/
+// To invoke:
+// curl -i --request POST 'http://localhost:54321/functions/v1/payment-sheet' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"amount":1150}'
